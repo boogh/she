e8d3498c597939db21cf4312b69cfaa79659ad53
boogh <Hammer@my\032macbook.speedport_w_724v_typ_a_05011603_00_009>
2017-01-21 23:40:19 +0100

    added HE2 and populationscript for project. migration does not habe problem

---
+++ SHE34/src/HE2/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from authtools.models import User
+from datetime import datetime
+# Create your models here.
+
+#
+# class HEset(models.Model):
+#     setList = (("1" , "Visibility of System Status"),
+#                 ("2" , "Match Between System and Real World"),
+#                 ("3","User Control and Freedom"),
+#                 ("4","Consistency and Standards"),
+#                 ("5", "Error Prevention"),
+#                 ("6", "Recognition Rather than Recall"),
+#                 ("7", "Flexibility and Efficiency of Use"),
+#                 ("8", "Aesthetic and Minimalistic Design"),
+#                 ("9", "Help Users Recognize, Diagnose, and Recover from Errors"),
+#                 ("10", "Help and Documentation"))
+#
+#
+#     name = models.CharField(max_length=50)
+#     set = models.CharField(max_length=200 , choices=setList)
+#
+#     def __str__(self):
+#         return self.name
+
+
+class Project(models.Model):
+    setList = (("1", "Visibility of System Status"),
+               ("2", "Match Between System and Real World"),
+               ("3", "User Control and Freedom"),
+               ("4", "Consistency and Standards"),
+               ("5", "Error Prevention"),
+               ("6", "Recognition Rather than Recall"),
+               ("7", "Flexibility and Efficiency of Use"),
+               ("8", "Aesthetic and Minimalistic Design"),
+               ("9", "Help Users Recognize, Diagnose, and Recover from Errors"),
+               ("10", "Help and Documentation"))
+
+    name = models.CharField(max_length=40)
+    link = models.URLField(blank=True)
+    place = models.CharField(max_length=100 , default='general')
+    description = models.TextField()
+    manager = models.ForeignKey(User,related_name="project_manager")
+    HE_Principle = models.CharField(max_length=300, choices= setList ,default="1")
+    evaluators = models.ManyToManyField(User , related_name="project_evaluator")
+    # deadline = models.DateField(default=time.date())
+    # creationTime = models.DateField(default=time.date())
+
+    def __str__(self):
+        return self.name
+
+# class Evaluation(models.Model):
+#     posOrNeg= (("p" , "Positive") , ("n" , "Negative"))
+#     severityList= (("1" , "No problem at all"), ("2","Cosmetic problem"),("3" ,"Minor usability problem") ,("4","Major usability problem"),("5","Catastrophic"))
+#     freq=(("1","almost never"),("2","rarely (< 10 % )") ,("3", "occasionally (11-50 %"),("4" ,"regularly(51-89 %"), ("5" , "constantly (>90 %"))
+#     for_project= models.ForeignKey(Project,related_name="evaluation_for_this_project",on_delete=models.CASCADE)
+#     evaluator = models.ForeignKey(Profile,related_name="evaluator",on_delete=models.CASCADE)
+#     heurPrincip= models.CharField(max_length=200)
+#     description = models.TextField()
+#     recommendation= models.TextField(blank=True)
+#     positivity = models.CharField(max_length=10, choices=posOrNeg , default="n")
+#     severity = models.CharField(max_length=10 , choices=severityList,default="1")
+#     frequency = models.CharField(max_length=10,choices=freq ,default="1")
+#     screenshot = models.ImageField(name="Screenshot" , upload_to='screenshots/%Y-%m-%d/',
+#                                 null=True,
+#                                 blank=True)
+#
+#
+#
+# class ListOfEvaluations(models.Model):
+#     name = models.CharField(max_length= 50)
+#     ofProject = models.ForeignKey(Project)
+#     fromUser= models.ForeignKey(Profile , related_name="project_manager")
+#     evaluations = models.ManyToManyField(Evaluation)
+#
+#     def __str__(self):
+#         return self.name
---
+++ SHE34/src/testpopultionsrc.py
@@ -0,0 +1,51 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SHE34.settings.base')
+
+import django
+django.setup()
+
+from authtools.models import User
+from HE.models import Project
+
+def populate():
+    user_items=[{'email': 'user1@user1.com', 'password': 'user1', 'name': 'user1'},
+                {'email': 'user2@user2.com', 'password': 'user2', 'name': 'user2'},
+                {'email': 'user3@user3.com', 'password': 'user3', 'name': 'user3'},
+                {'email': 'user4@user4.com', 'password': 'user4', 'name': 'user4'},
+                {'email': 'user5@user5.com', 'password': 'user5', 'name': 'user5'},
+                {'email': 'user6@user6.com', 'password': 'user6', 'name': 'user6'},
+                {'email': 'user7@user7.com', 'password': 'user7', 'name': 'user7'},
+                {'email': 'user8@user8.com', 'password': 'user8', 'name': 'user8'},]
+    project_items=[{'name':'p1', 'description' : ' This is project 1', 'manager':'user1',
+                    'evaluator' :['user2','user3','user4','user5' ] },
+                   {'name': 'p2', 'description': ' This is project 2', 'manager': 'user2',
+                    'evaluator': ['user1', 'user3', 'user4', 'user5']},
+                   {'name': 'p3', 'description': ' This is project 3', 'manager': 'user3',
+                    'evaluator': ['user1', 'user2', 'user4', 'user5']},
+                   {'name': 'p4', 'description': ' This is project 4', 'manager': 'user4',
+                    'evaluator': ['user1', 'user2', 'user3', 'user5']},
+                   {'name': 'p5', 'description': ' This is project 5', 'manager': 'user1',
+                    'evaluator': ['user2', 'user3', 'user4', 'user5']},
+                   {'name': 'p6', 'description': ' This is project 6', 'manager': 'user2',
+                    'evaluator': ['user1', 'user3', 'user4', 'user5']},
+                   {'name': 'p7', 'description': ' This is project 7', 'manager': 'user1',
+                    'evaluator': ['user2', 'user3', 'user4', 'user5']},
+                   ]
+    for user in user_items:
+        addUser(user)
+
+
+def addUser(userdata):
+    user = User.objects.get_or_create(email=userdata['email'] , password=userdata['password'] , name=userdata['name'])[0]
+    user.save()
+    print("Added User")
+
+def addProject(projectData):
+    project = Project.objects.get_or_create(name=projectData['name'], description = projectData['description'],
+                                            manager=User.objects.get(name=projectData['manager']))
+    for evaluator in projectData['evaluator']:
+        project.evaluators = User.objects.get(name=evaluator)
+
+if __name__ == '__main__':
+    print('Running population script...')
+    populate()
