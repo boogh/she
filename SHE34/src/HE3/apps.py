5f50ef7390a8bb75baac2f878cf6f2f3b93ab5e4
boogh <Hammer@my\032macbook.speedport_w_724v_typ_a_05011603_00_009>
2017-01-22 15:04:35 +0100

    added HE3/

---
+++ SHE34/src/HE3/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class He3Config(AppConfig):
+    name = 'HE3'
---
+++ SHE34/src/HE3/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template,Context
+from django.views import generic
+from HE3.models import Project
+# from django.contrib.auth.mixins import LoginRequiredMixin
+
+#
+def showDashboard(request):
+    model = Project
+    template_name = 'HE3/dashboard.html'
+    user = request.user
+    projects = Project.objects.all()
+    asmanager = projects.filter(manager=user.pk)
+    asevalutor = projects.filter(evaluators =user.pk)
+    context ={'asmanager' : asmanager , 'asevalutor' : asevalutor}
+
+    return render(request, template_name,context)
+
+
+
+
+# class ProjectList(generic.list.ListView):
+#     model = Project
+#     template_name = 'music/project_list.html'
+#     def get_queryset(self):
+#         user = self.request.user
+#         queryset = Project.objects.all()
+#         # queryset= super(ProjectList,self).get_queryset()
+#         queryset1= queryset.filter(manager=user.pk)
+#         # queryset2 = queryset.filter(evalutors = user.pk)
+#
+#         return queryset1
+#     def get_context_data(self, **kwargs):
+#         context =
+
+# class ProjectList(LoginRequiredMixin,generic.ListView):
+#     template_name = 'HE/dashboard.html'
+#     model = Project
