8a6e27fea0c17f51bbf45d6e80a1d5bdcbaaeab3
boogh <Hammer@my\032macbook.speedport_w_724v_typ_a_05011603_00_009>
2017-02-23 15:53:29 +0100

    added htmlexport

--- SHE34/src/SHE34/urls.py
+++ SHE34/src/SHE34/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 import profiles.urls
 import accounts.urls
-import Merge.urls
+import merg.urls
 from . import views
 
 urlpatterns = [
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^users/', include(profiles.urls, namespace='profiles')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(accounts.urls, namespace='accounts')),
-    url(r'^merge/',include(Merge.urls,namespace='merge'))
+    url(r'^merge/',include(merg.urls,namespace='merge'))
 ]
 
 # User-uploaded files like profile pics need to be served in development
