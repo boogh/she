ddf2bbd4a8624ab82469bc065f6aec8b752ae6fc
boogh <boogh313@gmail.com>
2017-06-05 02:35:38 +0200

    Added some new tags to mytags

--- SHE34/src/HE3/templatetags/mytags.py
+++ SHE34/src/HE3/templatetags/mytags.py
@@ -1,6 +1,19 @@
 from django import template
+from random import randint
 register = template.Library()
 
 @register.filter(name='addcss')
 def addcss(field, css):
    return field.as_widget(attrs={"class":css})
+
+@register.assignment_tag
+def evaluator_filter(evals, evaluator):
+   return evals.filter(evaluator= evaluator)
+
+@register.assignment_tag
+def random(id):
+    return randint(0,10000) + id
+
+@register.simple_tag
+def add(a , b):
+    return a+b
